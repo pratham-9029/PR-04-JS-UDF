565f9f64dbdcbf262978a1497e8913e4e0ea192d
pratham-9029 <saixerox5075@gmail.com>
2025-09-04 15:52:55 +0530

    Done

--- Recursive_Function/script.js
+++ Recursive_Function/script.js
@@ -1,7 +1,20 @@
-// let factValue = document.querySelector('input[name="factValue"]')
+let factValue = document.querySelector('input[name="factValue"]')
+let ans = document.querySelector('input[name="answer"]')
 
-const fact = (n) => {
-    if (n <= 1) return 1;
-    return n * fact(n - 1);
+let fact = (n) => {
+    if(n <= 1){
+        return 1;
+    }else{
+        return n*fact(n-1);
+    }
 }
-console.log(fact(9));
+
+let calculateFactorial = () =>{
+    let num = parseInt(factValue.value);
+    if (isNaN(num)) {
+        ans.value = "Enter a number";
+    } else {
+        ans.value = fact(num);
+    }
+    factValue.value = '';
+}
